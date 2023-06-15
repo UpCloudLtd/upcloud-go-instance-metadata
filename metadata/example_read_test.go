ad473ba43a5d27ec0a2d551346dda61ab832212f
Ville Välimäki <ville.valimaki@upcloud.com>
2023-06-15 15:04:31 +0300

    docs: error handling in read example

--- metadata/example_read_test.go
+++ metadata/example_read_test.go
@@ -2,11 +2,18 @@ package metadata_test
 
 import (
 	"log"
+	"os"
 
 	"github.com/UpCloudLtd/upcloud-go-instance-metadata/metadata"
 )
 
 func Example_read() {
-	m, _ := metadata.Read()
-	log.Printf("I'm at '%s' and my instance ID is '%s'", m.Region, m.InstanceID)
+	m, err := metadata.Read()
+	if err != nil {
+		log.Printf("read failed: %s", err.Error())
+		os.Exit(1)
+	}
+	if m != nil {
+		log.Printf("I'm at '%s' and my instance ID is '%s'", m.Region, m.InstanceID)
+	}
 }
